87ac16298b7f4e3e79ffe0717f8699b585a27d9a
MysterD <myster@d>
2022-06-03 22:16:07 -0700

    Cylboard SSL tree

--- levels/ttm/moon_smiley/geo.inc.c
+++ levels/ttm/moon_smiley/geo.inc.c
@@ -2,7 +2,6 @@
 const GeoLayout ttm_geo_000DBC[] = {
    GEO_CULLING_RADIUS(1000),
    GEO_OPEN_NODE(),
-      GEO_BILLBOARD(),
       GEO_OPEN_NODE(),
          GEO_SCALE(0x00, 32768),
          GEO_OPEN_NODE(),
